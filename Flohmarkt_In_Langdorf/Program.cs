adf39dc1e6c4b3891a8929c6e220b68fa82586e0
xThe Pain <natixbox@hotmail.de>
2021-04-18 15:40:06 +0200

    Done

--- Flohmarkt_In_Langdorf/Program.cs
+++ Flohmarkt_In_Langdorf/Program.cs
@@ -9,10 +9,11 @@ namespace Flohmarkt_In_Langdorf
 {
     class Program
     {
-        static List<string> input_txt = File.ReadAllLines(@"D:\Coding\BWINF\2. Runde\Aufgabe 1\Materialien\flohmarkt1.txt").ToList();
+        static List<string> input_txt = File.ReadAllLines(@"D:\Coding\BWINF\2. Runde\Aufgabe 1\Materialien\flohmarkt7.txt").ToList();
         static int Voranmeldungen = Convert.ToInt32(input_txt[0]);
         static List<string> Numbers = new List<string>();
         static List<string> SortedList = new List<string>();
+        static int[] Info_SortedList = new int[Voranmeldungen];
 
         //3 Zahlen -> Mietbeginn(volle stunden) | Mietende (volle Stunde) |länge des Standes (in Metern)
         //Flohmarkt Öffnungszeiten 8-18 | Flohmarkt länge = 1000m
@@ -42,10 +43,61 @@ namespace Flohmarkt_In_Langdorf
             }
             return 0;
         }
-        static int[] SortList_Index(int[] array)
+        static int GetMinIndex(int[] array, int i, int min)
+        {
+            int index = 0;
+            //Console.WriteLine("array: " + array[i] + " min: " + min + " i: " + i);
+            if (array[i] < min)
+            {
+                //Console.WriteLine("array2: " + array[i] + " min2: " + min + " i2: " + i);
+                min = array[i];
+                index = i;
+            }
+            return index;
+        }
+
+
+        static void SortList_Index_Fix(int[] array)
+        {
+            int[] num = new int[Voranmeldungen];
+            int[] num2 = new int[Voranmeldungen];
+            List<string> List = new List<string>();
+            List<string> Check_Num = new List<string>();
+
+            for (int i = 0; i < Voranmeldungen; i++)
+            {
+                num[i] = Convert.ToInt32(Numbers[i].Split(' ')[0] + Numbers[i].Split(' ')[1]);
+                num2[i] = Convert.ToInt32(Numbers[i].Split(' ')[0] + Numbers[i].Split(' ')[1]);
+
+            }
+            Array.Sort(num2);
+            int min = num[0];
+            for (int i = 0; i < Voranmeldungen; i++)
+            {
+                for (int j = 0; j < Voranmeldungen; j++)
+                {
+                    if (num[j] == num2[i] && !Check_Num.Contains(j.ToString())) //!tmp.Contains(j.ToString()))
+                    {
+                        List.Add(j.ToString());
+                        Check_Num.Add(j.ToString());
+                        //tmp += j.ToString();
+                    }
+                }
+                //Console.WriteLine("");
+            }
+            for (int i = 0; i < Voranmeldungen; i++)
+            {
+                array[i] = Convert.ToInt32(List[i]);
+                // Console.WriteLine("List: " + List[i]);
+            }
+        }
+
+        #region Old SortList_Index
+        /*static int[] SortList_Index(int[] array)
         {
             int[] num = new int[Voranmeldungen];
             int num_len = 0;
+
             for (int i = 0; i < Voranmeldungen; i++)
             {
                 num[i] = Convert.ToInt32(Numbers[i].Split(' ')[0] + Numbers[i].Split(' ')[1]);
@@ -58,6 +110,48 @@ namespace Flohmarkt_In_Langdorf
             }
             return array;
         }
+        */
+        #endregion
+
+
+
+
+
+
+        static int[] SortList_Index(int[] array)
+        {
+            int[] num = new int[Voranmeldungen];
+            int[] num2 = new int[Voranmeldungen];
+            List<string> List = new List<string>();
+            List<string> Check_Num = new List<string>();
+
+            for (int i = 0; i < Voranmeldungen; i++)
+            {
+                num[i] = Convert.ToInt32(Numbers[i].Split(' ')[0] + Numbers[i].Split(' ')[1]);
+                num2[i] = Convert.ToInt32(Numbers[i].Split(' ')[0] + Numbers[i].Split(' ')[1]);
+
+            }
+            Array.Sort(num2);
+            for (int i = 0; i < Voranmeldungen; i++)
+            {
+                for (int j = 0; j < Voranmeldungen; j++)
+                {
+                    if (num[j] == num2[i] && !Check_Num.Contains(j.ToString())) //!tmp.Contains(j.ToString()))
+                    {
+                        List.Add(j.ToString());
+                        Check_Num.Add(j.ToString());
+                    }
+                }
+                //Console.WriteLine("");
+            }
+
+            for (int i = 0; i < Voranmeldungen; i++)
+            {
+                array[i] = Convert.ToInt32(List[i]);
+                // Console.WriteLine("List: " + List[i]);
+            }
+            return array;
+        }
 
         static void SortList(List<string> List)
         {
@@ -65,19 +159,21 @@ namespace Flohmarkt_In_Langdorf
             for (int i = 0; i < Voranmeldungen; i++)
             {
                 List.Add(ReadSpecificLine(SortList_Index(SortedList)[i]));
+                //Console.WriteLine("SortList: " + SortedList[i]);
             }
         }
 
         static void InfoPrint()
         {
-            int[] SortedList = new int[Voranmeldungen];
             for (int i = 0; i < Voranmeldungen; i++)
             {
-                int Zeit = MathAbs_Cal(Convert.ToInt32(ReadSpecificLine(SortList_Index(SortedList)[i]).Split(' ')[0]), Convert.ToInt32(ReadSpecificLine(SortList_Index(SortedList)[i]).Split(' ')[1]));
-                int Kosten = Zeit * Convert.ToInt32(ReadSpecificLine(SortList_Index(SortedList)[i]).Split(' ')[2]);
+                string[] tmp = ReadSpecificLine(Info_SortedList[i]).Split(' ');
 
-                Console.WriteLine($"{i + 1}. Von {ReadSpecificLine(SortList_Index(SortedList)[i]).Split(' ')[0]} bis {ReadSpecificLine(SortList_Index(SortedList)[i]).Split(' ')[1]} Uhr | Mietlänge: {ReadSpecificLine(SortList_Index(SortedList)[i]).Split(' ')[2]}m");
-                Console.WriteLine($"=>Mietzeit: {Zeit}h| Kosten: {Kosten} Euro \n");
+                int Zeit = MathAbs_Cal(Convert.ToInt32(tmp[0]), Convert.ToInt32(tmp[1]));
+                int Kosten = Zeit * Convert.ToInt32(tmp[2]);
+
+                Console.WriteLine($"{i + 1}. Von {tmp[0]} bis {tmp[1]} Uhr | Mietlänge: {tmp[2]}m");
+                Console.WriteLine($"=>Mietzeit: {Zeit}h | Kosten: {Kosten} Euro \n");
             }
         }
 
@@ -98,16 +194,54 @@ namespace Flohmarkt_In_Langdorf
             }
         }
 
+        static int GesamtPreis(int Mietbegin, int Mietende, int Meter) // Mietende - Mietbegin = Stunden |  Stunden * Meter = Gesamtpreis
+        {
+            int Stunden = MathAbs_Cal(Mietbegin, Mietende);
+            return Stunden * Meter;
+        }
+
+        static string RemoveAfterChar(string input, string character)
+        {
+            int index = input.LastIndexOf(character);
+            if (index > 0)
+                input = input.Substring(0, index);
+            return input;
+        }
+
+        static void Remove_MinPreis(int MietlängenCheck, decimal min, List<string> List)
+        {
+            if (MietlängenCheck > 1000) // Den kleinsten Gesamtpreis entfernen
+            {
+                //Console.WriteLine("Den kleinsten Gesamtpreis entfernen\n");
+                for (int i = 0; i < List.Count; i++)
+                {
+                    int start_time = Convert.ToInt32(List[i].Split(' ')[0]);
+                    int end_time = Convert.ToInt32(List[i].Split(' ')[1]);
+                    int Meter = Convert.ToInt32(List[i].Split(' ')[2]);
+                    int Preis = GesamtPreis(start_time, end_time, Meter);
+                    //Console.WriteLine("Preis: " + Preis + " Min: " + min);
+                    if (Preis == min)
+                    {
+                        Console.WriteLine($"{List[i]} wird entfernt");
+                        List.Remove(List[i]);
+                    }
+                    else
+                    {
+                        //Console.WriteLine("Fehler!!!");
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             List<int> Test = new List<int>();
             AddNumbers();
+            SortList_Index_Fix(Info_SortedList);
             InfoPrint();
             SortList(SortedList);
             TimeCheck();
 
-            //int[] SortedList_Index = new int[Voranmeldungen];
-            //SortList_Index(SortedList_Index);
 
             #region Test
             /* for (int i = 0; i < Voranmeldungen; i++)
@@ -156,26 +290,85 @@ namespace Flohmarkt_In_Langdorf
              }*/
             #endregion
 
-
-
-            for (int i = 0; i < SortedList.Count; i++)
+            int sum = 0;
+            decimal min = Decimal.MaxValue;
+            List<string> Remove_nums = new List<string>();
+            for (int a = 0; a < SortedList.Count; a++)
             {
-                int start_time = Convert.ToInt32(SortedList[i].Split(' ')[0]);
-                int end_time = Convert.ToInt32(SortedList[i].Split(' ')[1]);
+                int Mietbegin = Convert.ToInt32(SortedList[a].Split(' ')[0]); //8;
+                int Mietende = Convert.ToInt32(SortedList[a].Split(' ')[1]);// 15;
+                int MietlängenCheck = 0;
+                //Console.WriteLine($"=>Mietbegin: {Mietbegin} | Mietende: {Mietende} \n");
 
-
-                //for (int a = 0; a < SortedList.Count; a++)
+                for (int Uhr = Mietbegin; Uhr < Mietende; Uhr++)
                 {
-                   // if (start_time >= 8 && start_time < 12)
+                    for (int i = 0; i < SortedList.Count; i++)
                     {
-                        Console.WriteLine($"{SortedList[i]}");
+                        int start_time = Convert.ToInt32(SortedList[i].Split(' ')[0]);
+                        int end_time = Convert.ToInt32(SortedList[i].Split(' ')[1]);
+                        int Meter = Convert.ToInt32(SortedList[i].Split(' ')[2]);
+
+                        //int Mietbegin = 8;
+                        //int Mietende = 12;
+
+                        if (end_time >= Mietbegin && end_time > Mietende || start_time < Mietende) //&& end_time >= Mietende)
+                        {
+                            int start_time_test = Convert.ToInt32(SortedList[i].Split(' ')[0]);
+                            int end_time_test = Convert.ToInt32(SortedList[i].Split(' ')[1]);
+                            //int Preis = GesamtPreis(start_time_test, end_time_test, Meter);
+
+                            //int Uhr = 9;
+                            if (start_time_test <= Uhr && end_time_test > Uhr && Uhr < Mietende)// Hier fehlt noch eine Schleife um alle Zeiten von start_time bis end_time abzuchecken
+                            {
+                                int Preis = GesamtPreis(start_time_test, end_time_test, Meter);
+                                //Console.WriteLine($"{SortedList[i]} {Preis}");
+                                if (Preis < min)// Kleinsten Gesamtpreis
+                                {
+                                    min = Preis;
+                                }
+                                //Console.WriteLine(min);
+                                //Remove_nums.Add($"{SortedList[i]} {Preis}");
+                                sum += Meter;
+                            }
+                        }
+                    }
+                    //Console.WriteLine($"Uhr:{Uhr}");
+                    //Console.WriteLine($"Länge:{sum}");
+                    //Console.WriteLine($"MinZahl: {min}");
+                    MietlängenCheck = sum;
+                    Remove_MinPreis(MietlängenCheck, min, SortedList);
+                    min = Decimal.MaxValue;
+                    sum = 0;
+                    //Console.WriteLine("");
+                }
+            }
+            //Console.WriteLine($"\nLänge:{sum}\n");
+            #region RemoveMin
+            /*if (MietlängenCheck > 1000) // Den kleinsten Gesamtpreis entfernen
+            {
+                Console.WriteLine("Den kleinsten Gesamtpreis entfernen");
+                for (int i = 0; i < SortedList.Count; i++)
+                {
+                    int start_time = Convert.ToInt32(SortedList[i].Split(' ')[0]);
+                    int end_time = Convert.ToInt32(SortedList[i].Split(' ')[1]);
+                    int Meter = Convert.ToInt32(SortedList[i].Split(' ')[2]);
+                    int Preis = GesamtPreis(start_time, end_time, Meter);
+                    if (Preis == min)
+                    {
+                        Console.WriteLine($"{SortedList[i]} wird entfernt");
+                        SortedList.Remove(SortedList[i]);
                     }
                 }
+            }*/
+            #endregion
 
+            Console.WriteLine("");
+            for (int i = 0; i < SortedList.Count; i++)
+            {
+                Console.WriteLine(SortedList[i]);
             }
 
             Console.ReadKey();
-
         }
     }
 }
